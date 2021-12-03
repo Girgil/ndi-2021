874b7c84506fa09fac0dc5345ca558812053e026
Bryan.BlinDorard <bryanblindorard@gmail.com>
2021-12-03 01:11:54 +0100

    petit avancement

--- app/app.py
+++ app/app.py
@@ -2,11 +2,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
+@app.route("/bateau")
+def bateau():
     return render_template(
-        "personne.html",
-        title = "bonjour à tous les amis 2")
+        "bateau.html",
+        title = "bonjour à tous les amis 2",
+        personnage_pas_important = ["Nom1", "Nom2", "Nom3"],
+        DATE = "01/01/01"
+        )
 
 
 if __name__ == "__main__":
