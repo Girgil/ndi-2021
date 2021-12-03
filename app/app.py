e71f8f78d5f94c232498324698ba8792519a9b89
Bryan.BlinDorard <bryanblindorard@gmail.com>
2021-12-03 04:37:14 +0100

    bato template

--- app/app.py
+++ app/app.py
@@ -7,8 +7,10 @@ def bateau():
     return render_template(
         "bateau.html",
         title = "bonjour à tous les amis 2",
-        personnage_pas_important = ["Nom1", "Nom2", "Nom3"],
-        DATE = "01/01/01"
+        personnage_pas_important = {"Patron":"NomPatron","Sous-Patron":"Nom du Sous-patron","armement":["Nom1", "Nom2", "Nom3"]},  
+        DATE = "01/01/01",
+        STATS_VIE = {"mort": 2, "vivant": 3},
+        NOM_BATEAU = "LES TROIS SOEURS"
         )
 
 
